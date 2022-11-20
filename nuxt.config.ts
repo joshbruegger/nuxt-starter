a92dbeff8cf47d83c96f0495f04b05d4efd8bdbc
Josh Bruegger <josh@bruegger.it>
2022-11-20 16:43:03 +0100

    feat: :sparkles: pinia

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,7 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   typescript: { shim: false },
-  modules: ["nuxt-windicss", "nuxt-headlessui"],
+  modules: ["nuxt-windicss", "nuxt-headlessui", "@pinia/nuxt"],
   headlessui: {
     prefix: "",
   },
