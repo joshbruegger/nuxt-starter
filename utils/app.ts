702962f17c2b8e3448d688376e1c059f5d19908e
Josh Bruegger <josh@bruegger.it>
2022-11-27 19:42:14 +0100

    fix: :bug: fixed eslint and prettier conflicts

--- utils/app.ts
+++ utils/app.ts
@@ -10,6 +10,7 @@ export function AppSetup() {
     `Uncaught NotFoundError: Failed to execute 'insertBefore' on 'Node': The node before which the new node is to be inserted is not a child of this node.`, // chromium based
     `NotFoundError: The object can not be found here.`, // safari
   ]
+
   if (typeof window !== 'undefined') {
     window.addEventListener('error', (ev) => {
       if (messages.includes(ev.message)) {
